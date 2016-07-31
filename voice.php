3ca7ced6d20c48a8835863de16b011873abe0993
usopyon <t.uehara@gmail.com>
2016-07-31 23:32:05 +0900

    新たに追加された音声のみサーバに問い合わせるよう仕様変更

--- voice.php
+++ voice.php
@@ -3,27 +3,39 @@
 $key = parse_ini_file("api.ini",true)["docomo"]["key"];
 
 if( isset($_GET["text"]) ){
-
     $text = $_GET["text"];
-    $apiUrl = "https://api.apigw.smt.docomo.ne.jp/voiceText/v1/textToSpeech?APIKEY=".$key;
-    //echo $apiUrl;
+    $fname = "voice/".md5($text);
 
-    $header = [
-        "Content-Type: application/x-www-form-urlencoded"
-    ];
-    $content = http_build_query([
-        "text"=>$text,
-        "speaker"=>"hikari"
-    ]);
-    $context = [
-        "http"=>[
-            "method"=>"POST",
-            "header"=>implode("¥r¥n",$header),
-            "content"=>$content
-        ]
-    ];
+    // ローカルファイルが存在するか確認
 
-    $response = file_get_contents($apiUrl,false,stream_context_create($context));
-    echo '<audio autoplay src="data:audio/wav;base64,'.base64_encode($response).'"/>';
-    echo $response;
+    if( file_exists($fname) ){
+        echo file_get_contents($fname);
+
+    }else{
+        $text = $_GET["text"];
+        $apiUrl = "https://api.apigw.smt.docomo.ne.jp/voiceText/v1/textToSpeech?APIKEY=".$key;
+        //echo $apiUrl;
+
+        $header = [
+            "Content-Type: application/x-www-form-urlencoded"
+        ];
+        $content = http_build_query([
+            "text"=>$text,
+            "speaker"=>"hikari"
+        ]);
+        $context = [
+            "http"=>[
+                "method"=>"POST",
+                "header"=>implode("¥r¥n",$header),
+                "content"=>$content
+            ]
+        ];
+
+        $response = file_get_contents($apiUrl,false,stream_context_create($context));
+        $contents = '<audio autoplay src="data:audio/wav;base64,'.base64_encode($response).'"/>';
+        echo $contents;
+        file_put_contents($fname,$contents);
+    }
+
+
 }
