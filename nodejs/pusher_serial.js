606ec72427429b704f9a4fc706c38922dc4118b6
usopyon <t.uehara@gmail.com>
2016-07-31 23:31:11 +0900

    node.jsでArduinoが接続されているポートを自動探索

--- nodejs/pusher_serial.js
+++ nodejs/pusher_serial.js
@@ -1,18 +1,31 @@
 var Pusher = require('pusher-js/node');
 var SerialPort = require('serialport');
+var conf = require('config');
+var fs = require('fs');
 
-var sp = new SerialPort('/dev/tty.usbmodem1411', {
-    baudRate: 115200
-});
+var sp = null;
 
-var pusher = new Pusher('558d88d3ce23e25aaf24', {
-    encrypted: true
-});
-
-var channel = pusher.subscribe('test_channel');
-channel.bind('my_event', function(data) {
-    console.log(data.message);
-    if( data.message.match(/^[0-9]{5}$/) ){
-        sp.write(data.message);
+var files = fs.readdirSync('/dev/');
+files.forEach(function(file){
+    if( file.match(/tty.usbmodem/) ){
+        sp = new SerialPort('/dev/'+file, {
+            baudRate: 115200
+        });
     }
-});
+})
+
+if( sp == null ){
+    console.log("disconnected");
+    return;
+}else{
+    var pusher = new Pusher(conf.app_id, {
+        encrypted: true
+    });
+    var channel = pusher.subscribe('test_channel');
+    channel.bind('my_event', function(data) {
+        console.log(data.message);
+        if( data.message.match(/^[0-9]{5}$/) ){
+            sp.write(data.message);
+        }
+    });
+}
